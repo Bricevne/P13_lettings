137dc90efa96b890813ea74c81231da9beec2669
Brice Venayre <brice.venayre@gmail.com>
2023-02-21 17:37:43 +0100

    refactor: add verbose name plural for address model

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -11,6 +11,9 @@ class Address(models.Model):
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
+    class Meta:
+        verbose_name_plural = "Addresses"
+
     def __str__(self):
         return f'{self.number} {self.street}'
 
