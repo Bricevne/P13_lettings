2840995242096f7aca3fcecadba04c853abe757e
Brice Venayre <brice.venayre@gmail.com>
2023-03-05 19:23:07 +0100

    feat: add deployment config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', default='secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'bve-oc-lettings-site.herokuapp.com']
 
 
 # Application definition
