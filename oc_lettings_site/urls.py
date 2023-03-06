6906878be57dd8ec59c0643f71a95ada191ddbdd
Brice Venayre <brice.venayre@gmail.com>
2023-03-06 17:15:24 +0100

    chore: remove sentry test

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,15 +4,9 @@ from django.urls import path, include
 from . import views
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-    return division_by_zero
-
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include("lettings.urls", namespace="lettings")),
     path('profiles/', include("profiles.urls", namespace="profiles")),
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
 ]
